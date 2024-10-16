0f591f1a0e6f2db553d11ef0f890594ea40ef129
maria-v-ch <m.chuvtaeva@gmail.com>
2024-10-16 13:46:48 +0000

    Update configuration for production deployment

--- user_directory/settings.py
+++ user_directory/settings.py
@@ -145,7 +145,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Add this line
-STATICFILES_DIRS = [BASE_DIR / "static"]  # Keep this line
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# Keep this line
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
