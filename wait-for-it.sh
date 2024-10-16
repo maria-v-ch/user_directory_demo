2e53bbadbcf83d951e24ea38d9ac83d5947cdf3f
maria-v-ch <m.chuvtaeva@gmail.com>
2024-10-16 17:18:04 +0000

    Update configuration for production deployment

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -8,7 +8,11 @@ shift
 cmd="$@"
 
 echo "Waiting for PostgreSQL to become available..."
-until PGPASSWORD=$DB_PASSWORD psql -h "$host" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
+echo "DB_USER: $DB_USER"
+echo "DB_NAME: $DB_NAME"
+echo "DB_HOST: $host"
+
+until PGPASSWORD=$DB_PASSWORD psql -h "$host" -U "$DB_USER" -d "$DB_NAME" -c '\l'; do
   >&2 echo "PostgreSQL is unavailable - sleeping"
   sleep 10
 done
