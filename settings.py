f9595b7162345617713eed5cf6efc2d39fe6b2b3
maria-v-ch <m.chuvtaeva@gmail.com>
2024-10-16 16:12:34 +0000

    Update configuration for production deployment

---
+++ settings.py
@@ -0,0 +1,18 @@
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'db',  # This should match the service name in docker-compose.yml
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
