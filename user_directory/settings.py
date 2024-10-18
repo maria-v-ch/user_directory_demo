77c42c994362dbdbfd21da317cb47a6c5d769db5
maria-v-ch <m.chuvtaeva@gmail.com>
2024-10-18 17:44:18 +0000

    fixing problems with docker-compose during Build and push Docker images

--- user_directory/settings.py
+++ user_directory/settings.py
@@ -136,9 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # Add this line
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-# Keep this line
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
